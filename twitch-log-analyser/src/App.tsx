1cc124d90d4348d229bd238c975549ad9de3baaf
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-02-07 01:47:49 +0100

    add boilerplate

---
+++ twitch-log-analyser/src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const App: React.FC = () => {
+  return (
+    <div className="App text-center bg-black text-white p-4">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ twitch-log-analyser/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+  return (
+    <div className="Header">
+      <h1 className="font-extrabold">Twitch Log Analyser</h1>
+    </div>
+  );
+};
+
+export default Header;
